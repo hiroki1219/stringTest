525ced096e44b2b02673698df9397a137552ef64
池田弘樹 <ikedahiroki@ikedahirokinoMacBook-puro.local>
2018-02-18 13:07:51 +0900

    new

---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main( void )
+{
+    char c[10];
+	std::string s;
+
+    std::cout << "string size   : " << s.size() << std::endl;
+    std::cout << "char[10] size : " << sizeof( c ) << std:: endl;
+    return 0;
+}
